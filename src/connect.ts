3936379152f2ecb6e777efdff2a5394a935cf8bc
Mikael Couzic <mcouzic.externe@pagesjaunes.fr>
2017-05-15 12:38:16 +0200

    v1.0.24 : Correct typo

--- src/connect.ts
+++ src/connect.ts
@@ -9,7 +9,7 @@ export function connect<IP>(WrappedComponent: React.SFC<IP>) {
 
    return {
       to: <EP>(internalProps$: Observable<IP>, options?: Partial<ConnectOptions<EP, IP>>): ConnectedComponent<EP> => connectTo(WrappedComponent, internalProps$, options),
-      WithMapper: <EP>(propsMapper: PropsMapper<EP, IP>, options?: Partial<ConnectOptions<EP, IP>>): ConnectedComponent<EP> => connectWithMapper(WrappedComponent, propsMapper, options)
+      withMapper: <EP>(propsMapper: PropsMapper<EP, IP>, options?: Partial<ConnectOptions<EP, IP>>): ConnectedComponent<EP> => connectWithMapper(WrappedComponent, propsMapper, options)
    }
 
 }
